847996ba7f6515298a666123d0a19d5cb5738af3
Stas Kuznetsov <stas@lipsat.ru>
2021-11-18 11:06:43 +0300

    Последние изменения. 18/11/2021

--- LESSON3/main.go
+++ LESSON3/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"time"
 )
 
 func main() {
-	Calc() //Вынес в отдельную функцию для простоты перезапуска
+	err := Calc()
+	if err != nil{
+		fmt.Println(err)
+	}
 }
 
-func Calc() {
+func Calc() error{
 	var a, b, res float64
 	var op string
 
@@ -33,9 +37,8 @@ func Calc() {
 	case "/":
 		res = a / b
 		if b == 0 {
-			fmt.Println("Ошибка на ноль делить нельзя")
-			Calc() //Тут я и хотел что бы при не верном вводе калькулятор перезапускался
-			break
+			//fmt.Println("Ошибка на ноль делить нельзя")
+			return errors.New("Ошибка на ноль делить нельзя")
 		}
 	case "sqrt":
 		res = math.Sqrt(float64(a))
@@ -48,27 +51,23 @@ func Calc() {
 			//если все числа до корня квадратные простые, то и после корня они простые экономим время
 			//в коде добавлен простой счетчик выполнения когда проходили полностью j/2  на вываолненеи 100000 понадобилось 13мс
 			//после оптимизации стало 6
-			if j%2 == 0 { // Оптимизация дала большое ускорение 87.101541ms
-				flag = false
-			} else {
 				m := int(math.Floor(math.Sqrt(float64(j))))
-				for i := 2; i <= m; i++ {
+				for i := 2; i <= m; i+=2 {
 					if j%i == 0 { //если число делится без остатка на i то оно не простое
 						flag = false
 						break
 					}
 				}
-			}
 			if flag == true {
 				fmt.Println(j, "- простое число")
 			}
 		}
 		fmt.Println("Проверка прошла за: ", time.Now().Sub(startAt)) //Высчитываем продолжительность проверки
 	default:
-		fmt.Println("Операция выбрана неверно")
-		Calc()
+		errors.New("Операция выбрана неверно")
 		break
 	}
 
 	fmt.Printf("Результат выполнения операции: %.4f\n", res)
+	return nil
 }
