4c8bc72ac8c5ab9af9db3590cec32f25c79f4475
Stas Kuznetsov <stas@lipsat.ru>
2021-11-22 22:58:55 +0300

    Home Work 7 Kuznetsov Stas

---
+++ LESSON7/Calc/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+//Value - Структура вводных данных
+type Value struct {
+	a float64
+	b float64
+}
+//Addition - метод сложения
+func (s *Value) Addition() float64 {
+	return s.a + s.b
+}
+//Subtraction - метод вычитания
+func (s *Value) Subtraction() float64 {
+	return s.a - s.b
+}
+//Multiplication - метод умножения
+func (s *Value) Multiplication() float64 {
+	return s.a * s.b
+}
+//Division - метод деления
+func (s *Value) Division() float64 {
+	return s.a / s.b
+}
+//Square - метод кореньквадратный
+func (s *Value) Square() float64 {
+	return math.Sqrt(float64(s.a))
+}
+//AreaCalculated - интерфейс для функций калькулятора
+type AreaCalculated interface {
+	Addition() float64
+	Subtraction() float64
+	Multiplication() float64
+	Division() float64
+	Square() float64
+}
+
+
+func main()  {
+	var d, e, res float64
+	var op string
+
+	fmt.Print("Введите первое число: ")
+	fmt.Scanln(&d)
+
+	fmt.Print("Введите второе число: ")
+	fmt.Scanln(&e)
+
+	fmt.Print("Введите операцию (+, -, *, /, sqrt, ^, quit): ")
+	fmt.Scanln(&op)
+	a := &Value{a: d, b: e}
+	switch op {
+	case "+":
+		res = AreaCalculated.Addition(a)
+	case "-":
+		res = AreaCalculated.Subtraction(a)
+	case "*":
+		res = AreaCalculated.Multiplication(a)
+	case "/":
+		if e == 0 {
+			fmt.Println("Ошибка на ноль делить нельзя")
+			return
+		}
+		res = AreaCalculated.Division(a)
+	case "sqrt":
+		res = AreaCalculated.Square(a)
+	case "quit":
+		os.Exit(1)
+	default:
+		fmt.Println("Операция выбрана неверно")
+		break
+	}
+	fmt.Printf("Результат выполнения операции: %.2f\n", res)
+}
