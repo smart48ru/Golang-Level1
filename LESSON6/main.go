eced6d64a5e3b080d6a045af200a2b4825375c09
Stas Kuznetsov <stas@lipsat.ru>
2021-11-22 13:42:17 +0300

    HW6 Kuznetsov Stas

--- LESSON6/main.go
+++ LESSON6/main.go
@@ -1,6 +1,5 @@
 package main
 
-<<<<<<< HEAD
 import "fmt"
 
 func main() {
@@ -37,42 +36,6 @@ func main() {
 	v := 5
 	d := 2
 
-	res := v*d
+	res := v * d
 	fmt.Println(res)
-=======
-import (
-	"fmt"
-	"math"
-)
-
-
-
-type Square struct {
-	Side float32
-}
-
-func (s *Square) CalculateArea() float32 {
-	return s.Side * s.Side
-}
-
-type Circle struct {
-	Radius float32
-}
-
-func (c *Circle) CalculateArea() float32 {
-	return math.Pi * c.Radius * c.Radius
-}
-
-type AreaCalculator interface {
-	CalculateArea() float32
-}
-
-func main() {
-	square := &Square{Side: 4}
-	circle := &Circle{Radius: 4}
-
-	for _, shape := range []AreaCalculator{square, circle} {
-		fmt.Println(shape.CalculateArea())
-	}
->>>>>>> main
 }
